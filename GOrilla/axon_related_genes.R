971fb25c70902ceefee48e2c037b673bc150b50c
Juan Ochoteco <jochotecoa@gmail.com>
2020-06-15 22:17:02 +0200

    Update 15-Jun

---
+++ GOrilla/axon_related_genes.R
@@ -0,0 +1,136 @@
+compare_boxpl <- function(df, attrib, cols.x, cols.y) {
+  if (!is.null(attrib)) {
+    gene_trt = df %>% 
+      dplyr::select(starts_with(attrib))
+    
+  } else {
+    gene_trt = df
+  }
+  
+  max_y = max(gene_trt, na.rm = T)
+  
+  gene_trt_untr = gene_trt %>% 
+    dplyr::select(matches(cols.x)) %>% 
+    unlist()
+  
+  gene_trt_tox = gene_trt %>% 
+    dplyr::select(matches(cols.y)) %>% 
+    unlist()
+  
+  newdf = cbind.data.frame(gene_trt_untr, gene_trt_tox)
+  colnames(newdf) = c(paste0(cols.x, attrib), paste0(cols.y, attrib))
+  boxplot(newdf, 
+          ylim = c(0, max_y))
+  
+}
+subSet = function(x, pattern) {
+  x[grep(pattern = pattern, x = x)]
+}
+
+
+library('VennDiagram')
+setwd("/share/script/hecatos/juantxo/analysis_trc")
+source('functions_JOA.R')
+forceLibrary(c('dplyr', 'tibble'))
+
+
+
+if (!exists('trt_proteomx')) {
+  comp = '5FU'
+  source('/share/script/hecatos/juantxo/analysis_trc/p.values_multiomics/checkSeveralOmicsTrTFile_JOA.R')
+}
+
+forceSetWd('/share/analysis/hecatos/juantxo/Score/analysis/GOrilla')
+
+gene_details = trt_proteomx %>% 
+  filter(external_gene_name == 'PAFAH1B1')
+
+
+# TPM ---------------------------------------------------------------------
+
+compare_boxpl(gene_details, 'targetRNA_TPM_')
+# TRT ---------------------------------------------------------------------
+
+compare_boxpl(gene_details, 'TrT_0.1_')
+
+# miRNA regulation --------------------------------------------------------
+
+par(mfrow = c(3, 2))
+
+compare_boxpl(gene_details, 'sum_miRNA_TPM', 'UNTR', 'Tox')
+
+compare_boxpl(gene_details, 'X.miRNAs')
+
+compare_boxpl(gene_details, 'E_circ_N')
+
+atts = gene_details %>% 
+  dplyr::select(contains('UNTR_072_1')) %>% 
+  colnames() %>% 
+  gsub('_UNTR_072_1_TrT', '', .) %>% 
+  .[-c(2, 17)]
+
+for (variable in atts) {
+  
+  compare_boxpl(gene_details, variable)
+  
+  readline(prompt="Press [enter] to continue")
+}
+
+
+setwd('~/Downloads')
+mirtarbase = read.csv('hsa_MTI.csv')
+
+mirnas_related = mirtarbase %>% 
+  filter(Target.Gene == 'PAFAH1B1',
+         Support.Type != 'Non-Functional MTI',
+         Support.Type != 'Non-Functional MTI (Weak)') %>% 
+  dplyr::select(miRNA) %>% 
+  unlist()
+
+dim(mirnas_related)
+
+mirna_dir = '/share/analysis/hecatos/juantxo/Score/input/miRNA_miRge2/'
+
+setwd(mirna_dir)
+setwd('5FU')
+mirna_5FU = read.csv('miR.RPM.csv')
+
+rows_exp = NULL
+for (variable in mirnas_related) {
+  row_exp = grep(pattern = variable, x = mirna_5FU$miRNA)
+  
+  if (length(row_exp) > 0) {
+    rows_exp = c(rows_exp, row_exp)
+  }
+  
+}
+
+mirnas_related_exp_5FU = mirna_5FU[rows_exp, ] %>% 
+  dplyr::select(matches('Tox|miRNA'), 
+                -matches('002_3|168'))
+
+
+setwd(mirna_dir)
+setwd('UNTR')
+mirna_UNTR = read.csv('miR.RPM.csv')
+
+rows_exp = NULL
+for (variable in mirnas_related) {
+  row_exp = grep(pattern = variable, x = mirna_UNTR$miRNA)
+  
+  if (length(row_exp) > 0) {
+    rows_exp = c(rows_exp, row_exp)
+  }
+  
+}
+
+mirnas_related_exp_UNTR = mirna_UNTR[rows_exp, ] %>% 
+  dplyr::select(-matches('002_3|168|240|336'))
+
+mirnas_related_exp = merge.data.frame(mirnas_related_exp_UNTR, 
+                                      mirnas_related_exp_5FU, 
+                                      by = 'miRNA')
+
+
+
+compare_boxpl(mirnas_related_exp, cols.x = 'UNTR')
